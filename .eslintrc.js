8f72add9c0e2928829c36550df4adaf0e7b0625b
Chris Alexander <chris@synaptechgenesis.com>
2023-01-06 18:46:36 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'no-continue': 'off',
+    'no-nested-ternary': 'off',
+    'no-console': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.ts'],
+      rules: {
+        'max-classes-per-file': 'off',
+      },
+    },
+  ],
+};
