aa2943deffc12206a4b2008f5f9bae5f7a3f8d78
Chris Alexander <chris@synaptechgenesis.com>
2023-01-07 12:06:02 -0500

    chore: env setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     sourceType: 'module',
@@ -7,14 +13,10 @@ module.exports = {
     'airbnb-base',
     'airbnb-typescript/base',
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
