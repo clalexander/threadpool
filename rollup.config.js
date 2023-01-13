802892028ec3ccea146275c0b487329c87c5c25d
Chris Alexander <chris@synaptechgenesis.com>
2023-01-13 16:11:09 -0500

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,26 @@
-import resolve from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import zip from 'rollup-plugin-zip';
+import commandLineArgs from 'command-line-args';
+import servicesConfig from './services.json';
+
+const optionDefinitions = [
+  {
+    name: 'only',
+    type: String,
+    multiple: true,
+  },
+];
+const options = commandLineArgs(optionDefinitions, { partial: true });
 
 const defaultConfig = {
   plugins: [
-    resolve(),
+    nodeResolve({
+      preferBuiltins: true,
+    }),
     typescript(),
     commonjs({
       requireReturnsDefault: 'auto',
@@ -24,20 +37,19 @@ const defaultConfig = {
   ],
 };
 
-const entries = [
-  {
+const services = servicesConfig.services || [];
+const onlyServices = options.only || services;
+const entries = services
+  .filter((service) => onlyServices.includes(service))
+  .map((service) => ({
     ...defaultConfig,
     plugins: [
       ...defaultConfig.plugins,
       zip({
-        file: 'test.zip',
+        file: `${service}.zip`,
       }),
     ],
-    input: 'services/test/index.ts',
-    output: {
-      ...defaultConfig.output,
-    },
-  },
-];
+    input: `services/${service}/index.ts`,
+  }));
 
 export default entries;
