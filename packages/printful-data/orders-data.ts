cfabf7106054054aabf9c2d7e65433ba515da37e
Chris Alexander <chris@synaptechgenesis.com>
2023-01-17 15:58:04 -0500

    feat: add printful orders data

---
+++ packages/printful-data/orders-data.ts
@@ -0,0 +1,46 @@
+import { DynamoDBDataProvider, ItemAttribute, QuerySpec } from 'aws-utils';
+import { Order } from 'printful';
+
+export interface PrintfulOrderKeyOptions {
+  id: number;
+  store: number;
+}
+
+export type PrintfulOrderQueryOptions = {
+  external_id: string;
+};
+
+export const PrintfulOrderAttributes: ItemAttribute[] = [
+  {
+    name: 'id',
+    type: 'N',
+  },
+  {
+    name: 'store',
+    type: 'N',
+  },
+  {
+    name: 'external_id',
+    type: 'S',
+  },
+];
+
+export const PrintfulOrderQuerySpecs: QuerySpec[] = [
+  {
+    index: 'ExternalId',
+    keyConditionExpression: 'external_id = :eid',
+    expressionAttributeValues: {
+      external_id: ':eid',
+    },
+  },
+];
+
+export class PrintfulOrdersData extends DynamoDBDataProvider<
+Order,
+PrintfulOrderKeyOptions,
+PrintfulOrderQueryOptions
+> {
+  constructor(tableName: string) {
+    super(tableName, PrintfulOrderAttributes, PrintfulOrderQuerySpecs);
+  }
+}
