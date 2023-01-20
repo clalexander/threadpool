c7bb7c13df29b0b9270eea65ecc5901ca68c6da6
Chris Alexander <chris@synaptechgenesis.com>
2023-01-20 13:57:00 -0500

    fix: fix failed service event type

--- services/inksoft-orders-sync/events.ts
+++ services/inksoft-orders-sync/events.ts
@@ -1,7 +1,7 @@
 export enum ServiceEventType {
   Started = 'inksoft_orders_sync.service.started',
   Completed = 'inksoft_orders_sync.service.completed',
-  Failed = 'inksoft_orders_sync.service.',
+  Failed = 'inksoft_orders_sync.service.failed',
 }
 
 export enum OrderSummaryEventType {
