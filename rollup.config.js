1d027457932db842e38c4f3b54598ff17ee9a4bd
Chris Alexander <chris@synaptechgenesis.com>
2023-01-24 16:18:11 -0500

    fis: add onwarn to bypass superjson rollup error

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,13 @@ const defaultConfig = {
   external: [
     'aws-sdk',
   ],
+  // @ts-ignore
+  onwarn(warning, warn) {
+    // skip this is undefined warning for superjson
+    if (warning.code === 'THIS_IS_UNDEFINED' && /superjson/.test(warning.id)) return;
+    // default handler otherwise
+    warn(warning);
+  },
   plugins: [
     nodeResolve({
       preferBuiltins: true,
